45fbcea1fa58fd331726848f7b0c1d3f5032f3f4
lilyflowr <ibadiela@gmail.com>
2023-07-24 14:19:42 +0100

    SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- lists all records of the table second_table of the database hbtn_0c_0 in MY MySQL server.
 -- Don’t list rows without a name value
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
